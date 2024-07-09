1a1b2aef88d7b851d36b7a2b2abeec8c8d350a36
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-07-09 20:12:25 +0200

    feat: hook

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+
+def init_git_repository():
+    current_dir = os.getcwd()
+    subprocess.run(['git', 'init'], cwd=current_dir)
+    print('Git repository initialized.')
+
+if __name__ == '__main__':
+    init_git_repository()
