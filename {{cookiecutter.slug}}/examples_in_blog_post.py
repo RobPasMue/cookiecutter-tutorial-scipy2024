6ee07f6fab5366d378a43b78c126d428488b1666
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-07-09 17:37:45 +0200

    feat: remove enum specific code

--- {{cookiecutter.slug}}/examples_in_blog_post.py
+++ {{cookiecutter.slug}}/examples_in_blog_post.py
@@ -1,17 +1,5 @@
-from enum import Enum
-
-
-class Color(Enum):
-    BLUE = 1
-    AZUL = 1
-    RED = 2
-    GREEN = 3
-
-
 def main():
-    print(Color.BLUE)
-    print(Color.AZUL)
-    print(Color.GREEN)
+    print()
 
 
 if __name__ == "__main__":
--- {{cookiecutter.slug}}/explore_enum_with_alias.py
+++ {{cookiecutter.slug}}/explore_enum_with_alias.py
@@ -1,22 +1,5 @@
-from enum import Enum
-
-
-class Color(Enum):
-    BLUE = 1
-    AZUL = 1
-    RED = 2
-    GREEN = 3
-
-
 def main():
-    print(list(Color))
-    print(Color.__members__)
-
-    # Accessing members
-    print(Color.BLUE)
-    print(Color.AZUL)
-    print(Color.GREEN)
-
+    pass
 
 if __name__ == "__main__":
     main()
