3982830acaa4cd66168286b9fb0cf0e13101f899
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-07-09 20:14:15 +0200

    fix: commit files from hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,8 @@ import subprocess
 def init_git_repository():
     current_dir = os.getcwd()
     subprocess.run(['git', 'init'], cwd=current_dir)
+    subprocess.run(['git', 'add', '.'], cwd=current_dir)
+    subprocess.run(['git', 'commit', '-m', 'Initial commit'], cwd=current_dir)
     print('Git repository initialized.')
 
 if __name__ == '__main__':
